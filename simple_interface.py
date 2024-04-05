b410bceff0f17686a6de794a08d803dbf318f8a1
sledhead <bigskyprogrammers@gmail.com>
2024-04-05 05:45:08 +0000

    testing more code

--- simple_interface.py
+++ simple_interface.py
@@ -1,3 +1,10 @@
 
 from AES_GCM import *
 
+#send in simple text to encrypt.
+my_special_text = "little sheep on the farm"
+cool_password = "cool1234"
+
+ret_stuff = encrypt(my_special_text, cool_password)
+
+print(ret_stuff)
