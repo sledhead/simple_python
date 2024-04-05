e9bb77da61f2a5f446c9ec78c5684a347f0d0568
sledhead <bigskyprogrammers@gmail.com>
2024-04-05 05:55:35 +0000

    getting stuff out

--- simple_interface.py
+++ simple_interface.py
@@ -5,6 +5,11 @@ from AES_GCM import *
 my_special_text = "little sheep on the farm"
 cool_password = "cool1234"
 
-ret_stuff = encrypt(my_special_text, cool_password)
+ret_stuff = encrypt(my_special_text.encode(), cool_password)
 
 print(ret_stuff)
+
+
+plain_stuff = decrypt(ret_stuff, cool_password)
+
+print(plain_stuff)
